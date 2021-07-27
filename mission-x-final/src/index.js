a7deaa0edf606f2c2601fef1fbecd2cb5763a2d4
GorettiA <gorettialani@gmail.com>
2021-07-27 15:16:54 +1200

    updated pointer cursor on header

--- mission-x-final/src/index.js
+++ mission-x-final/src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
-ReactDOM.render(
-
-    <App />,
-
+ReactDOM.render(<App />,
+  
   document.getElementById('root')
-=======
-ReactDOM.render(<App />,document.getElementById('root')
->>>>>>> a4393f5542918672dfda5a6b53e8b8f3eace5f0b
+
 );
 
